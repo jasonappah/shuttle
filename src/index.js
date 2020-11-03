d9332c90e2c7f3e873760e375655b2fcc0c716ec
Jason Antwi-Appah <hey@jasonaa.me>
2020-11-03 22:03:34 +0000

    chore: remove perf monitoring

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import { GeistProvider, CssBaseline } from '@geist-ui/react'
 
 ReactDOM.render(
