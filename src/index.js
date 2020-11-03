6825e13368e20cff46ffe43ce7b193cbb5e8e9a8
Jason Antwi-Appah <hey@jasonaa.me>
2020-11-03 22:29:24 +0000

    chore: added firebase config, reactifire, switched to react concurrent mode

--- src/index.js
+++ src/index.js
@@ -3,16 +3,43 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { GeistProvider, CssBaseline } from '@geist-ui/react'
+import { FirebaseAppProvider } from 'reactfire';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <GeistProvider>
-    <CssBaseline />
-    <App />
-    </GeistProvider>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+var firebaseConfig = {
+  apiKey: "AIzaSyAJWjAVVzVCYz3SIjd1k8yryuuEfMSERw8",
+  authDomain: "shuttle-87812.firebaseapp.com",
+  databaseURL: "https://shuttle-87812.firebaseio.com",
+  projectId: "shuttle-87812",
+  storageBucket: "shuttle-87812.appspot.com",
+  messagingSenderId: "511076742812",
+  appId: "1:511076742812:web:74054c3600f0e8fd9cdf59"
+};
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <GeistProvider>
+//       <FirebaseAppProvider firebaseConfig={firebaseConfig}>
+//     <CssBaseline />
+//     <App />
+//     </FirebaseAppProvider>
+//     </GeistProvider>
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+// Enable Concurrent Mode
+// https://reactjs.org/docs/concurrent-mode-adoption.html#enabling-concurrent-mode
+createRoot(document.getElementById("root")).render(
+<React.StrictMode>
+  <GeistProvider>
+    <FirebaseAppProvider firebaseConfig={firebaseConfig}>
+  <CssBaseline />
+  <App />
+  </FirebaseAppProvider>
+  </GeistProvider>
+</React.StrictMode>);
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
